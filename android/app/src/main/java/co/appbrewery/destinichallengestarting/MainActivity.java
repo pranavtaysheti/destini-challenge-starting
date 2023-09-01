1c0a51784734cca43d099e4afe45f934f4f314d3
pranavtaysheti <pranavtaysheti@gmail.com>
2023-09-01 15:50:05 +0530

    fixed - app builds for android

--- android/app/src/main/java/co/appbrewery/destinichallengestarting/MainActivity.java
+++ android/app/src/main/java/co/appbrewery/destinichallengestarting/MainActivity.java
@@ -1,6 +1,6 @@
 package co.appbrewery.destinichallengestarting;
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
 
